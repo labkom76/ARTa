bd67d92d824c0e206730313ef6ceffb794f4708d
[dyad] <labinformatika16@gmail.com>
2025-10-13 15:16:22 +0800

    [dyad] wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "next-themes"; // Import ThemeProvider
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
@@ -38,37 +39,39 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <SessionContextProvider>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/print-verifikasi" element={<PrintVerifikasi />} />
-            <Route path="/print-koreksi" element={<PrintKoreksi />} />
-            <Route path="/lengkapi-profil" element={<LengkapiProfil />} />
-            <Route path="/verifikasi-dokumen/:tagihanId" element={<VerifikasiDokumen />} /> {/* New public route */}
-            <Route element={<MainLayout />}>
-              <Route path="/" element={<Index />} />
-              <Route path="/dashboard-skpd" element={<DashboardSKPD />} />
-              <Route path="/portal-skpd" element={<PortalSKPD />} />
-              <Route path="/dashboard-registrasi" element={<DashboardRegistrasi />} />
-              <Route path="/portal-registrasi" element={<PortalRegistrasi />} />
-              <Route path="/riwayat-registrasi" element={<RiwayatRegistrasi />} />
-              <Route path="/dashboard-verifikasi" element={<DashboardVerifikasi />} />
-              <Route path="/portal-verifikasi" element={<PortalVerifikasi />} />
-              <Route path="/riwayat-verifikasi" element={<RiwayatVerifikasi />} />
-              <Route path="/dashboard-koreksi" element={<DashboardKoreksi />} />
-              <Route path="/rekap-dikembalikan" element={<RekapDikembalikan />} />
-              <Route path="/admin/users" element={<AdminUsers />} />
-              <Route path="/admin/dashboard" element={<AdminDashboard />} />
-              <Route path="/admin/tagihan" element={<AdminTagihan />} />
-              <Route path="/admin/custom-login" element={<AdminCustomLogin />} />
-              <Route path="/admin/kode-skpd" element={<AdminKodeSKPD />} />
-              <Route path="/admin/jadwal-penganggaran" element={<AdminJadwalPenganggaran />} />
-              <Route path="/admin/kode-wilayah" element={<AdminKodeWilayah />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            </Route>
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </SessionContextProvider>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem> {/* ThemeProvider added here */}
+          <SessionContextProvider>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/print-verifikasi" element={<PrintVerifikasi />} />
+              <Route path="/print-koreksi" element={<PrintKoreksi />} />
+              <Route path="/lengkapi-profil" element={<LengkapiProfil />} />
+              <Route path="/verifikasi-dokumen/:tagihanId" element={<VerifikasiDokumen />} /> {/* New public route */}
+              <Route element={<MainLayout />}>
+                <Route path="/" element={<Index />} />
+                <Route path="/dashboard-skpd" element={<DashboardSKPD />} />
+                <Route path="/portal-skpd" element={<PortalSKPD />} />
+                <Route path="/dashboard-registrasi" element={<DashboardRegistrasi />} />
+                <Route path="/portal-registrasi" element={<PortalRegistrasi />} />
+                <Route path="/riwayat-registrasi" element={<RiwayatRegistrasi />} />
+                <Route path="/dashboard-verifikasi" element={<DashboardVerifikasi />} />
+                <Route path="/portal-verifikasi" element={<PortalVerifikasi />} />
+                <Route path="/riwayat-verifikasi" element={<RiwayatVerifikasi />} />
+                <Route path="/dashboard-koreksi" element={<DashboardKoreksi />} />
+                <Route path="/rekap-dikembalikan" element={<RekapDikembalikan />} />
+                <Route path="/admin/users" element={<AdminUsers />} />
+                <Route path="/admin/dashboard" element={<AdminDashboard />} />
+                <Route path="/admin/tagihan" element={<AdminTagihan />} />
+                <Route path="/admin/custom-login" element={<AdminCustomLogin />} />
+                <Route path="/admin/kode-skpd" element={<AdminKodeSKPD />} />
+                <Route path="/admin/jadwal-penganggaran" element={<AdminJadwalPenganggaran />} />
+                <Route path="/admin/kode-wilayah" element={<AdminKodeWilayah />} />
+                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              </Route>
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </SessionContextProvider>
+        </ThemeProvider>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
